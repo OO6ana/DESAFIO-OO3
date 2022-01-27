a1e3166955f4bb4e293405806611dc51db51ea93
OO6ana <analuizacoelhodemoura343@gmail.com>
2022-01-27 20:27:36 -0300

    Desafio finalizado

---
+++ script.js
@@ -0,0 +1,17 @@
+//1 - Criar um loop que conte de 1 até 10 usando FOR
+
+for(a = 1; a <= 10; a++)
+console.log(a)
+
+//2 - Criar um loop que conte de 10 até 1 usando WHILE
+
+
+//3 - Criar um loop que conte todos os números ímpares de 1 até 100
+
+for(a = 1; a <= 100; a += 2)
+console.log(a)
+
+//4- Criar um loop que conte todos os números pares de 0 a 100
+
+for(a = 0; a <= 100; a += 2)
+console.log(a)
